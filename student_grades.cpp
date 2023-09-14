11d0759209dfeb9bbecff49d858a728c96091017
WillNoSmittt <89476597+WillNoSmittt@users.noreply.github.com>
2023-09-14 16:34:22 -0400

    student_grades.cpp

--- student_grades.cpp
+++ student_grades.cpp
@@ -18,5 +18,28 @@ int main ()
   ifstream InputFile;
   ofstream OutputFile;
 
+  InputFile.open("student_grades.txt");
 
+  if(!InputFile)
+  {
+    cout<<"Error: the path or files does not exist"<<endl;
+    exit(1);
+  }
+
+  InputFile >> student1_name;
+  InputFile >> student1_test1 >> student1_test2 >> student1_test3;
+
+  InputFile >> student2_name;
+  InputFile >> student2_test1 >> student2_test2 >> student2_test3;
+  
+InputFile.close();
+  
+  Aver_student1 = (student1_test1 + student1_test2 + student1_test3)/3;
+  Aver_student2 = (student2_test1 + student2_test2 + student2_test3)/3;
+
+  OutputFile.open("Final_grade.txt")
+  OutputFile >> student1_name >>" Final Grade: " << Aver_student1;
+  OutputFile >> student2_name >>" Final Grade: " << Aver_student2;  
+
+  OutputFile.close();
 }
